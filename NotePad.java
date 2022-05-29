b01243367bfcc75c032ab0d24bafd836e8e201e5
Divyansh Agrawal <57246976+DivyanshByte@users.noreply.github.com>
2022-05-29 10:56:40 +0530

    Removed NetBeans Header

--- NotePad.java
+++ NotePad.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author byte
- */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
